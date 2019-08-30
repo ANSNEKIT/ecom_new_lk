c06dd8d780d074084c185e93a7ee5538108a64ed
ansnekit <ansnekit@gmail.com>
2019-08-30 20:02:52 +0400

    Добавлен скрипт для активации агентских платежей

---
+++ source/js/index.js
@@ -0,0 +1,24 @@
+let agentGroup = document.getElementsByClassName("agentGoods");
+let isAgentTrue = document.getElementById("isAgentTrue");
+let isAgentFalse = document.getElementById("isAgentFalse");
+let agentTrueCheckbox = document.getElementById("AgentTrue");
+let agentFalseCheckbox = document.getElementById("AgentFalse");
+
+var checkYes = () => {
+    if (agentTrueCheckbox.checked === true) {
+        agentGroup[0].removeAttribute("disabled");
+    } else {
+        agentGroup[0].setAttribute("disabled", "");
+    }
+};
+
+var checkNo = () => {
+    if (agentFalseCheckbox.checked === true) {
+        agentGroup[0].setAttribute("disabled", "");
+    } else {
+        agentGroup[0].removeAttribute("disabled");
+    }
+};
+
+isAgentTrue.addEventListener("click", checkYes);
+isAgentFalse.addEventListener("click", checkNo);
