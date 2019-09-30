f6ba5f046c294b56365da8d4445c93dc244ff589
ansnekit <ansnekit@gmail.com>
2019-09-30 11:17:54 +0300

    Исправлена страница store.html Добавлена иконка статуса связи

--- source/js/store.js
+++ source/js/store.js
@@ -1,4 +1,21 @@
-$companyBlock = document.querySelector('#accordion');
+let $companyBlock = document.querySelector('#accordion');
+let $questItem = document.querySelectorAll('.questItem');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    for (let i = 0; i < $questItem.length; i++) {
+        let statusFiscal = $questItem[i].querySelector('p[data-js-id="status"]').textContent;
+        let $statusFiscalImg = $questItem[i].querySelector('span[data-js-id="status-img"]');
+        if (statusFiscal === 'Давно не было связи') {
+            $statusFiscalImg.innerHTML = '<img src="img/no-wifi.png" alt="Нет связи с кассой">'
+        } else {
+            $statusFiscalImg.innerHTML = '<img src="img/wifi.png" alt="Касса в сети">'
+        }
+    }
+
+});
+
+
 
 $companyBlock.addEventListener('click', (evt) => {
     let $elem = evt.target;
