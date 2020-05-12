5f3b3c7c5b020f5f9d726ab754cbe8f4f3ae6f87
ansnekit <ansnekit@gmail.com>
2020-05-13 01:18:02 +0300

    Исправлена валидация формы

--- source/js/validate.js
+++ source/js/validate.js
@@ -1,3 +1,20 @@
+'use strict';
+
+(function () {
+    window.addEventListener('load', function () {
+        var $forms = document.querySelectorAll('.needs-validation');
+        var validation = Array.prototype.filter.call($forms, function ($form) {
+            $form.addEventListener('submit', function (event) {
+                if ($form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                $form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
 function validateInn(inn, error) {
     var result = false;
     if (typeof inn === 'number') {
@@ -43,5 +60,4 @@ function validateInn(inn, error) {
         }
     }
     return result;
-}
-
+};
