4078c6bdef6b263b86a3aba7e00ebb8bd5a7ef9d
ansnekit <ansnekit@gmail.com>
2020-08-18 20:31:14 +0300

    fix btn clear

--- source/js/add-integration.js
+++ source/js/add-integration.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const $inpEmail = document.querySelector('#login');
-const $inpPass = document.querySelector('#passwd');
+const $inpEmail = document.getElementById('login');
+const $inpPass = document.getElementById('passwd');
+const $inpUrl = document.getElementById('redirect_url');
 const $btnClear = document.querySelector('button[data-js-id="btnClear"]');
 
 $btnClear.addEventListener('click', () => {
   $inpEmail.value = '';
   $inpPass.value = '';
+  $inpUrl.value = '';
 });
 
